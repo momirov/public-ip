a459f5b65e0fc9de66863fd92f9205bd18367125
Vladimir Momirov <thezmaj@gmail.com>
2014-07-05 12:58:04 +0200

    Get port from environment variable.

--- public-ip.go
+++ public-ip.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", handlerText)
 	http.HandleFunc("/json", handlerJson)
-	http.ListenAndServe(":8083", nil)
+	http.ListenAndServe(GetPort(), nil)
+}
+
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "8083"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
 
 func handlerText(w http.ResponseWriter, r *http.Request) {
